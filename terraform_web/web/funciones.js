f68593521af2438998587b7a1f79fb9a5738996e
unknown <daniel05jimenez357@gmail.com>
2025-06-03 09:09:56 +0200

    prueba de despliegue

---
+++ terraform_web/web/funciones.js
@@ -0,0 +1,19 @@
+
+/**
+ * Representa la primera funcion
+ */
+function primera_funcion(){
+    console.log("primera funcion")
+}
+/**
+ * Representa la segunda funcion
+ */
+function segunda_funcion(){
+    console.log("segunda funcion")
+}
+/**
+ * Representa la primera funcion
+ */
+function tercera_funcion(){
+    console.log("tercera funcion")
+}
